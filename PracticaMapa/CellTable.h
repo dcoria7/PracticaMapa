929710602e83f7df6d7631d08ea3b292c22eba51
D C <dc@mbp-de-d.gateway.2wire.net>
2014-03-27 03:40:41 -0600

    first commit

---
+++ PracticaMapa/CellTable.h
@@ -0,0 +1,13 @@
+//
+//  CellTable.h
+//  PracticaMapa
+//
+//  Created by D C on 3/26/14.
+//  Copyright (c) 2014 D C. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CellTable : UITableViewController
+
+@end
---
+++ PracticaMapa/MapViewController.h
@@ -0,0 +1,13 @@
+//
+//  MapViewController.h
+//  PracticaMapa
+//
+//  Created by D C on 3/27/14.
+//  Copyright (c) 2014 D C. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MapViewController : UIViewController
+
+@end
